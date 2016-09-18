5f683086a67caac1538182e24e576ff59b3067e1
Vitalii Viazovoi <vitvyaz@gmail.com>
2016-09-18 20:14:21 +0300

    Added logging

--- src/main/java/ua/vitvyaz/helloworld/HelloWorld.java
+++ src/main/java/ua/vitvyaz/helloworld/HelloWorld.java
@@ -1,7 +1,10 @@
+package ua.vitvyaz.helloworld;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
@@ -13,17 +16,29 @@ public class HelloWorld {
     ResourceBundle res;
 
     HelloWorld(){
-        LOG.trace("Constructor HelloWorld()");
+        LOG.trace("Constructor ua.vitvyaz.helloworld.HelloWorld()");
+        LOG.trace("read data from file helloworld.properties");
         res = ResourceBundle.getBundle("helloworld");
     }
 
     public static void main(String[] args) {
+        Locale defaultLocale = Locale.getDefault();
+        System.out.println(defaultLocale);
+        Locale.setDefault(new Locale("en"));
+        System.out.println(Locale.getDefault());
+
+
         LOG.traceEntry("method main()");
+
         LocalDateTime now = LocalDateTime.now();
         LOG.info("now = " + now.toLocalDate() + " " + now.toLocalTime());
+
         HelloWorld helloWorld = new HelloWorld();
         System.out.println(helloWorld.getMessage(now));
         LOG.trace("exit from main()");
+
+        Locale.setDefault(defaultLocale);
+        System.out.println(Locale.getDefault());
     }
 
     public String getMessage(LocalDateTime time) {
